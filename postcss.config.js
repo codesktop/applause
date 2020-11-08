8d1941d7941233d9d0576393ff1ebe5561893930
codesktop <codesktop@foxmail.com>
2020-11-08 23:34:03 +0800

    init commit

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      purge: [],
+      darkMode: false, // or 'media' or 'class'
+      theme: {
+        extend: {}
+      },
+      variants: {
+        extend: {}
+      },
+      plugins: []
+    },
+    autoprefixer: {}
+  }
+}
