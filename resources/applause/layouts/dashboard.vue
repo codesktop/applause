7883fd6246c541fbf18bd3344a87049e4838f0a4
codesktop <codesktop@foxmail.com>
2020-11-11 19:09:21 +0800

    init commit

--- resources/applause/layouts/dashboard.vue
+++ resources/applause/layouts/dashboard.vue
@@ -1,7 +1,71 @@
 <template>
+  <a-layout>
+    <a-layout-sider class="h-screen" width="240">
+      <a-menu v-model:openKeys="openKeys" v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item>
+          MANAGE
+        </a-menu-item>
+        <a-sub-menu>
+          <template #title>
+            <span>ACCOUNT</span>
+          </template>
+          <a-menu-item>
+            Billing
+          </a-menu-item>
+          <a-menu-item>
+            Settings
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header class="bg-white shadow">
+        <div class="flex justify-between">
+          <div>
+            <a-input-search></a-input-search>
+          </div>
+          <a-dropdown placement="bottomRight">
+            <div class="flex items-center cursor-pointer px-3">
+              <a-avatar src="" size="large"></a-avatar>
+            </div>
+            <template #overlay>
+              <a-menu>
+                <a-menu-item>
+                  <router-link to="/user/profile">Your profile</router-link>
+                </a-menu-item>
+                <a-menu-item>
+                  <router-link to="/settings">Settings</router-link>
+                </a-menu-item>
+                <a-menu-item>
+                  <router-link to="/">Sign out</router-link>
+                </a-menu-item>
+              </a-menu>
+            </template>
+          </a-dropdown>
+        </div>
+      </a-layout-header>
+      <a-layout-content>
+
+      </a-layout-content>
+      <a-layout-footer>
+
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
+import { DownOutlined } from '@ant-design/icons-vue'
+
 export default {
+  data() {
+    return {
+      openKeys: [],
+      selectedKeys: []
+    }
+  },
+  components: {
+    DownOutlined
+  }
 }
 </script>
